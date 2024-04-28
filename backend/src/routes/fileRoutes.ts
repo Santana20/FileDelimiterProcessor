fb7deb753d77f33d6b83bcd015e69e506d0012c3
Sebastian Contreras <sstfmscontreras@gmail.com>
2024-04-27 22:36:33 -0500

    change backend to serverless

--- backend/src/routes/fileRoutes.ts
+++ backend/src/routes/fileRoutes.ts
@@ -1,41 +1,59 @@
 import express from 'express';
 import multer from 'multer';
+import archiver from 'archiver';
 
 const router = express.Router();
 const upload = multer();
 
-let memoryFile: Express.Multer.File | undefined = undefined;
-let filteredFileBuffer: Buffer
-let removedFileBuffer: Buffer
+function getFileNameAndExtension(nombreArchivo: string) {
+    const parts = nombreArchivo.split('.');
+    const filename = parts.slice(0, -1).join('.');
+    const extension = parts.slice(-1)[0];
 
-// Ruta para cargar archivos
-router.post('/upload', upload.single('file'), (req, res) => {
-    const { file: updloadFile } = req
+    return {
+        filename,
+        extension
+    };
+}
 
-    if (!updloadFile) {
-        return res.status(400).send('No se ha proporcionado ningún archivo');
-    }
+router.post('/subir-archivo', upload.single('archivo'), (req, res) => {
+    // Acceder al archivo subido
+    const archivo = req.file;
 
-    memoryFile = updloadFile
+    // Acceder a los datos en formato JSON del body
+    const datosJSON = req.body;
 
-    // Devolver confirmación de carga exitosa
-    res.status(200).send('Archivo cargado exitosamente');
+    // Hacer lo que necesites con el archivo y los datos JSON
+    console.log('Archivo recibido:', archivo);
+    console.log('Datos JSON recibidos:', datosJSON);
+
+    // Enviar una respuesta
+    res.send('Archivo y datos recibidos correctamente');
 });
 
 // Ruta para eliminar líneas del archivo en memoria
-router.post('/remove-rows', (req, res) => {
-    if (!memoryFile) {
-        return res.status(400).send('No se ha proporcionado ningún archivo. Cargue un archivo en /file/upload');
+router.post('/remove-rows', upload.single('file'), (req, res) => {
+
+    const { file: uploadFile } = req
+
+    if (!uploadFile) {
+        return res.status(400).send('No se ha proporcionado ningún archivo');
     }
 
-    console.log(req.body)
-    // Obtener el arreglo de filas a eliminar del cuerpo de la solicitud
-    const rowsToRemove: number[] = req.body.rowsToRemove;
+    console.log(req.body.body)
+
+    const bodyParse = JSON.parse(req.body.body);
+    console.log(bodyParse)
+
+    // Obtener el arreglo de filas a eliminar y el delimiter del cuerpo de la solicitud
+    const rowsToRemove: number[] = bodyParse.rowsToRemove;
+    const delimiter: string = bodyParse.delimiter || '|';
+
     if (!rowsToRemove || !Array.isArray(rowsToRemove)) {
         return res.status(400).send('Se espera un arreglo de filas a eliminar');
     }
 
-    const fileBuffer = memoryFile.buffer;
+    const fileBuffer = uploadFile.buffer;
     const fileContent = fileBuffer.toString('utf-8');
     const fileLines = fileContent.split('\n');
 
@@ -44,7 +62,7 @@ router.post('/remove-rows', (req, res) => {
         if (rowsToRemove.includes(index + 1)) {
             acc.removedLines.push(line);
         } else {
-            acc.filteredLines.push(`${acc.filteredLines.length + 1}|${line.split('|').slice(1).join('|')}`);
+            acc.filteredLines.push(`${acc.filteredLines.length + 1}${delimiter}${line.split(delimiter).slice(1).join(delimiter)}`);
         }
         return acc;
     }, { filteredLines: [] as string[], removedLines: [] as string[] });
@@ -53,34 +71,27 @@ router.post('/remove-rows', (req, res) => {
     const filteredContent = filteredLines.join('\n');
     const removedContent = removedLines.join('\n');
 
-    // Crear archivos adjuntos con el contenido
-    filteredFileBuffer = Buffer.from(filteredContent, 'utf-8');
-    removedFileBuffer = Buffer.from(removedContent, 'utf-8');
+    // Obtenemos el nombre y extension del archivo
+    const { filename, extension } = getFileNameAndExtension(uploadFile.originalname)
 
-    res.status(200).send('Se realizo las operaciones correctamente')
+    // Establecer encabezados para indicar que se enviarán archivos
+    res.setHeader('Content-Type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', `attachment; filename="${filename}_compress.zip"`);
+
+    // Crear un archivo ZIP
+    const archivoZip = archiver('zip', { zlib: { level: 9 } });
+
+    // Transmitir el archivo ZIP al cliente
+    archivoZip.pipe(res);
+
+    // Añadir el contenido filtrado al archivo ZIP
+    archivoZip.append(filteredContent, { name: `${filename}_filtered.${extension}` });
+
+    // Añadir el contenido removido al archivo ZIP
+    archivoZip.append(removedContent, { name: `${filename}_removed.${extension}` });
+
+    // Finalizar el archivo ZIP
+    archivoZip.finalize();
 });
-
-// Endpoint para descargar el archivo de líneas filtradas
-router.get('/download-filtered-lines', (req, res) => {
-    if (!filteredFileBuffer) {
-        return res.status(404).send('No se encontró el archivo de líneas filtradas');
-    }
-
-    res.set('Content-Type', 'text/plain');
-    res.attachment('file_filtered_lines.txt');
-    res.send(filteredFileBuffer);
-});
-
-// Endpoint para descargar el archivo de líneas eliminadas
-router.get('/download-removed-lines', (req, res) => {
-    if (!removedFileBuffer) {
-        return res.status(404).send('No se encontró el archivo de líneas eliminadas');
-    }
-
-    res.set('Content-Type', 'text/plain');
-    res.attachment('file_removed_lines.txt');
-    res.send(removedFileBuffer);
-});
-
 
 export default router;
